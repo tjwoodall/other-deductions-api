9c21972ff31e22d480511e3e4ca799e8e98b31f2
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2020-06-30 10:29:38 +0100

    [MTDSA-6065] - Retrieve other deductions parser & validator (#20)
    * started parser & validator
    
    * finished parser & validator + specs

---
+++ test/v1/mocks/validators/MockRetrieveOtherDeductionsValidator.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.mocks.validators
+
+import org.scalamock.handlers.CallHandler1
+import org.scalamock.scalatest.MockFactory
+import v1.controllers.requestParsers.validators.RetrieveOtherDeductionsValidator
+import v1.models.errors.MtdError
+import v1.models.request.retrieveOtherDeductions.RetrieveOtherDeductionsRawData
+
+class MockRetrieveOtherDeductionsValidator extends MockFactory {
+
+  val mockValidator: RetrieveOtherDeductionsValidator = mock[RetrieveOtherDeductionsValidator]
+
+  object MockRetrieveOtherDeductionsValidator {
+
+    def validate(data: RetrieveOtherDeductionsRawData): CallHandler1[RetrieveOtherDeductionsRawData, List[MtdError]] = {
+      (mockValidator
+        .validate(_: RetrieveOtherDeductionsRawData))
+        .expects(data)
+    }
+  }
+}
