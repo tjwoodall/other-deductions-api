190aecb1b589bece67eebaf4db593c361bec6b87
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2020-07-01 08:26:40 +0100

    [MTDSA-6067] - retrieve other deductions controller, routes, & ITs (#21)
    * controler routes & ITs
    
    * ITs completed
    
    * removed spaces in routes file

---
+++ test/v1/controllers/RetrieveOtherDeductionsControllerSpec.scala
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers
+
+import play.api.libs.json.Json
+import uk.gov.hmrc.domain.Nino
+import play.api.mvc.Result
+import uk.gov.hmrc.http.HeaderCarrier
+import v1.mocks.hateoas.MockHateoasFactory
+import v1.mocks.requestParsers.MockRetrieveOtherDeductionsRequestParser
+import v1.mocks.services.{MockAuditService, MockEnrolmentsAuthService, MockMtdIdLookupService, MockRetrieveOtherDeductionsService}
+import v1.models.errors._
+import v1.models.hateoas.{HateoasWrapper, Link}
+import v1.models.hateoas.Method.GET
+import v1.models.outcomes.ResponseWrapper
+import v1.models.request.retrieveOtherDeductions.{RetrieveOtherDeductionsRawData, RetrieveOtherDeductionsRequest}
+import v1.models.response.retrieveOtherDeductions.{RetrieveOtherDeductionsHateoasData, RetrieveOtherDeductionsResponse, Seafarers}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class RetrieveOtherDeductionsControllerSpec
+  extends ControllerBaseSpec
+    with MockEnrolmentsAuthService
+    with MockMtdIdLookupService
+    with MockRetrieveOtherDeductionsService
+    with MockRetrieveOtherDeductionsRequestParser
+    with MockHateoasFactory
+    with MockAuditService {
+
+  trait Test {
+    val hc = HeaderCarrier()
+
+    val controller = new RetrieveOtherDeductionsController(
+      authService = mockEnrolmentsAuthService,
+      lookupService = mockMtdIdLookupService,
+      parser = mockRetrieveOtherDeductionsRequestParser,
+      service = mockRetrieveOtherDeductionsService,
+      hateoasFactory = mockHateoasFactory,
+      cc = cc,
+    )
+
+    MockedMtdIdLookupService.lookup(nino).returns(Future.successful(Right("test-mtd-id")))
+    MockedEnrolmentsAuthService.authoriseUser()
+  }
+
+  private val nino = "AA123456A"
+  private val taxYear = "2019-20"
+  private val correlationId = "X-123"
+
+  private val rawData = RetrieveOtherDeductionsRawData(nino, taxYear)
+  private val requestData = RetrieveOtherDeductionsRequest(Nino(nino), taxYear)
+
+  private val testHateoasLink = Link(href = s"/individuals/deductions/other/{nino}/{taxYear}", method = GET, rel = "self")
+
+  private val responseBody = RetrieveOtherDeductionsResponse(
+    Some(Seq(Seafarers(
+      Some("myRef"),
+      2000.99,
+      "Blue Bell",
+      "2018-04-06",
+      "2019-04-06"
+    )))
+  )
+
+  "handleRequest" should {
+    "return Ok" when {
+      "the request received is valid" in new Test {
+
+        MockRetrieveOtherDeductionsRequestParser
+          .parse(rawData)
+          .returns(Right(requestData))
+
+        MockRetrieveOtherDeductionsService
+          .retrieve(requestData)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, responseBody))))
+
+        MockHateoasFactory
+          .wrap(responseBody, RetrieveOtherDeductionsHateoasData(nino, taxYear))
+          .returns(HateoasWrapper(responseBody, Seq(testHateoasLink)))
+
+        val result: Future[Result] = controller.handleRequest(nino, taxYear)(fakeRequest)
+        status(result) shouldBe OK
+        header("X-CorrelationId", result) shouldBe Some(correlationId)
+      }
+    }
+    "return the error as per spec" when {
+      "parser errors occur" should {
+        def errorsFromParserTester(error: MtdError, expectedStatus: Int): Unit = {
+          s"a ${error.code} error is returned from the parser" in new Test {
+
+            MockRetrieveOtherDeductionsRequestParser
+              .parse(rawData)
+              .returns(Left(ErrorWrapper(Some(correlationId), error, None)))
+
+            val result: Future[Result] = controller.handleRequest(nino,taxYear)(fakeRequest)
+
+            status(result) shouldBe expectedStatus
+            contentAsJson(result) shouldBe Json.toJson(error)
+            header("X-CorrelationId", result) shouldBe Some(correlationId)
+          }
+        }
+
+        val input = Seq(
+          (BadRequestError, BAD_REQUEST),
+          (NinoFormatError, BAD_REQUEST),
+          (TaxYearFormatError, BAD_REQUEST),
+          (RuleTaxYearRangeInvalidError, BAD_REQUEST)
+        )
+
+        input.foreach(args => (errorsFromParserTester _).tupled(args))
+      }
+      "service errors occur" should {
+        def serviceErrors(mtdError: MtdError, expectedStatus: Int): Unit = {
+          s"a $mtdError error is returned from the service" in new Test {
+
+            MockRetrieveOtherDeductionsRequestParser
+              .parse(rawData)
+              .returns(Right(requestData))
+
+            MockRetrieveOtherDeductionsService
+              .retrieve(requestData)
+              .returns(Future.successful(Left(ErrorWrapper(Some(correlationId), mtdError))))
+
+            val result: Future[Result] = controller.handleRequest(nino, taxYear)(fakeRequest)
+
+            status(result) shouldBe expectedStatus
+            contentAsJson(result) shouldBe Json.toJson(mtdError)
+            header("X-CorrelationId", result) shouldBe Some(correlationId)
+          }
+        }
+
+        val input = Seq(
+          (NinoFormatError, BAD_REQUEST),
+          (TaxYearFormatError, BAD_REQUEST),
+          (NotFoundError, NOT_FOUND),
+          (DownstreamError, INTERNAL_SERVER_ERROR)
+        )
+
+        input.foreach(args => (serviceErrors _).tupled(args))
+      }
+    }
+  }
+}
