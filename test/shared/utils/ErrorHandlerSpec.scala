354b5102bf4de1ceeff8b464b6131bb029c6d39e
Ste Ward <15212062+stephenkward@users.noreply.github.com>
2026-06-09 12:16:32 +0100

    MTDSA-34749 - Handle GatewayTimeoutException as 504 GATEWAY_TIMEOUT (#140)
    * MTDSA-34749 - Handle GatewayTimeoutException as 504 GATEWAY_TIMEOUT
    
    * Tidy up

--- test/shared/utils/ErrorHandlerSpec.scala
+++ test/shared/utils/ErrorHandlerSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,7 +40,7 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
 
   "onClientError" should {
     "return 404 with error body" when {
-      s"URI not found" in new Test {
+      "URI not found" in new Test {
 
         val result: Future[Result] = handler.onClientError(requestHeader, Status.NOT_FOUND, "test")
         status(result).shouldBe(Status.NOT_FOUND)
@@ -116,7 +116,6 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
     "return 401 with error body" when {
       "AuthorisationException thrown" in new Test {
         val result: Future[Result] = handler.onServerError(requestHeader, new InsufficientEnrolments("test") with NoStackTrace)
-        // TODO This really should be FORBIDDEN (403), but would need to be changed across all the APIs at once (if at all).
         status(result).shouldBe(UNAUTHORIZED)
 
         contentAsJson(result) shouldBe ClientOrAgentNotAuthorisedError.asJson
@@ -132,7 +131,7 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
         contentAsJson(result) shouldBe BadRequestError.asJson
       }
 
-      "Upstream4xxResponse thrown" in new Test() {
+      "Upstream4xxResponse thrown" in new Test {
         val ex: UpstreamErrorResponse = UpstreamErrorResponse("client error", TOO_MANY_REQUESTS, TOO_MANY_REQUESTS, None.orNull)
         val result: Future[Result]    = handler.onServerError(requestHeader, ex)
 
@@ -149,7 +148,7 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
         contentAsJson(result) shouldBe InternalError.asJson
       }
 
-      "Upstream5xxResponse thrown" in new Test() {
+      "Upstream5xxResponse thrown" in new Test {
         val ex: UpstreamErrorResponse = UpstreamErrorResponse("server error", SERVICE_UNAVAILABLE, SERVICE_UNAVAILABLE, None.orNull)
         val result: Future[Result]    = handler.onServerError(requestHeader, ex)
 
@@ -159,6 +158,13 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
     }
 
     "return GATEWAY_TIMEOUT with error body" when {
+      "GatewayTimeoutException is thrown" in new Test {
+        val result: Future[Result] = handler.onServerError(requestHeader, new GatewayTimeoutException("test") with NoStackTrace)
+
+        status(result) shouldBe GATEWAY_TIMEOUT
+        contentAsJson(result) shouldBe GatewayTimeoutError.asJson
+      }
+
       Seq(499, GATEWAY_TIMEOUT).foreach { statusCode =>
         s"a $statusCode UpstreamErrorResponse is returned" in new Test {
           val errorResponse: UpstreamErrorResponse = UpstreamErrorResponse("request timeout", statusCode, statusCode, Map.empty)
@@ -173,17 +179,16 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
 
   def anyVersionHeader: (String, String) = ACCEPT -> s"application/vnd.hmrc.1.0+json"
 
-  class Test {
-    val method = "some-method"
-
+  private trait Test {
     val requestHeader: FakeRequest[AnyContent] = FakeRequest().withHeaders(anyVersionHeader)
 
-    val auditConnector: AuditConnector = mock[AuditConnector]
-    val httpAuditEvent: HttpAuditEvent = mock[HttpAuditEvent]
+    private val auditConnector: AuditConnector = mock[AuditConnector]
 
-    val eventTags: Map[String, String] = Map("transactionName" -> "event.transactionName")
+    private val httpAuditEvent: HttpAuditEvent = mock[HttpAuditEvent]
 
-    val dataEvent: DataEvent = DataEvent(
+    private val eventTags: Map[String, String] = Map("transactionName" -> "event.transactionName")
+
+    private val dataEvent: DataEvent = DataEvent(
       auditSource = "auditSource",
       auditType = "event.auditType",
       eventId = "",
@@ -202,7 +207,7 @@ class ErrorHandlerSpec extends UnitSpec with GuiceOneAppPerSuite {
       .expects(*, *, *)
       .returns(Future.successful(Success))
 
-    val configuration: Configuration = Configuration(
+    private val configuration: Configuration = Configuration(
       "appName"                                         -> "myApp",
       "bootstrap.errorHandler.warnOnly.statusCodes"     -> List(OK),
       "bootstrap.errorHandler.suppress4xxErrorMessages" -> false,
